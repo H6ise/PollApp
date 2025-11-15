bb384a7a735862b0de9d9946a1e9f15a6f5b30ca
H9ise <116332542+H6ise@users.noreply.github.com>
2025-11-15 19:33:40 +0300

    Добовления страниц и стилей к ним

---
+++ PollApp/wwwroot/js/signalr.js
@@ -0,0 +1,10 @@
+﻿// wwwroot/js/signalr.js
+// Optional separate file for SignalR client setup, but since integrated in views, can be omitted or used globally if needed
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/resultsHub")
+    .withAutomaticReconnect()
+    .build();
+
+connection.start().catch(err => console.error(err));
+
+// Export if modular, but for simplicity, include in views
--- PollApp/wwwroot/js/site.js
+++ PollApp/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// wwwroot/js/site.js
+// Custom JavaScript for the application
+// Includes AJAX helpers, form enhancements, etc.
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    // Example: Global AJAX error handling
+    $(document).ajaxError(function (event, jqxhr, settings, thrownError) {
+        console.error("AJAX Error: " + thrownError);
+        alert("An error occurred. Please try again.");
+    });
+
+    // If needed, add more global behaviors
+});
+
+// Function to add option (can be used in Create/Edit if not inline in views)
+function addOption(containerId) {
+    let optionIndex = $(`#${containerId} .input-group`).length;
+    const newOption = `
+        <div class="input-group mb-2">
+            <input type="text" name="Options[${optionIndex}]" class="form-control" placeholder="New Option" required />
+            <button type="button" class="btn btn-danger" onclick="removeOption(this)">Remove</button>
+        </div>`;
+    $(`#${containerId}`).append(newOption);
+}
+
+function removeOption(button) {
+    $(button).parent().remove();
+}
+
+// If separating SignalR logic, but since it's in views, optional
